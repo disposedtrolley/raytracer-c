67775e36155a8a9e698df4f872277929db4e7673
James Liu <james@jamesliu.io>
2021-03-19 13:33:51 +1100

    feat: build and link core lib

---
+++ lib/core/tuples.c
@@ -0,0 +1,5 @@
+#include "tuples.h"
+
+int my_tuple_func() {
+    return 2;
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <core/tuples.h>
 
 int main() {
     printf("Hello Raytracer\n");
+    printf("%d\n", my_tuple_func());
 }
